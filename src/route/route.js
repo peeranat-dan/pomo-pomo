bc35c340be119539b1eee592b45e2a45571cbeaf
Nin <ppeeranat.d@gmail.com>
2022-02-05 13:36:46 +0700

    - Change web icon - Add error page - ad _redirects for netlify to deploy SPA

--- src/route/route.js
+++ src/route/route.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router"; // import function to create router and history
 // import Vue components
 import Home from '../views/Home.vue';
+import Error from '../views/Error.vue';
 
 const routes = [ // declare routes
     { path: '/', redirect: '/home' },
-    { path: '/home', name: 'Home', component: Home }
+    { path: '/home', name: 'Home', component: Home },
+    { path: '/:pathMatch(.*)*', name: 'Error', component: Error}
 ];
 
 const router = createRouter({
---
+++ src/views/Error.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bg-slate-800 flex h-screen w-screen">
+    <div class="m-auto">
+      <h1 class="text-6xl font-bold text-white">404 Not found</h1>
+      <h3 class="text-xl text-center text-gray-200">Back to <router-link class="text-rose-400 font-semibold" to="/home">home</router-link></h3>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
